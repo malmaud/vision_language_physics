aec46ae79727b2443a78b2bc7fcfb6f54fdc89d9
Jon Malmaud <malmaud@gmail.com>
2016-05-31 13:27:46 -0400

    Docker files

--- src/main.jl
+++ src/main.jl
@@ -14,18 +14,3 @@ compute_distance(b1::Box, b2::Box) = compute_distance(center(b1), center(b2))
 function compute_distance(p1::Point, p2::Point)
     return sqrt((p1.x-p2.x)^2 + (p1.y-p2.y)^2)
 end
-
-using .Words
-include("words/CloseMod.jl")
-using .CloseMod
-
-
-include("a.jl")
-include("b.jl")
-
-display(ModA.A(1))
-display(ModB.B(1))
-
-super(ModA.A) == super(ModB.B)
-
-super(ModA.A)
