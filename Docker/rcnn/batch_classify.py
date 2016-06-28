c7c583fa3ba83a367c5f01b423057441f17ed07f
Jon Malmaud <malmaud@gmail.com>
2016-06-28 15:12:35 -0400

    Docker batch script for extracting detections

---
+++ Docker/rcnn/batch_classify.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import _init_paths
+from fast_rcnn.config import cfg
+from fast_rcnn.test import im_detect
+from fast_rcnn.nms_wrapper import nms
+from utils.timer import Timer
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+import caffe, os, sys, cv2
+import argparse
+import SocketServer
+import struct
+import csv
+import re
+
+
+class JArgs:
+    demo_net = 'vgg16'
+    cpu_mode = False
+    gpu_id = 0
+
+CLASSES = ('__background__',
+           'aeroplane', 'bicycle', 'bird', 'boat',
+           'bottle', 'bus', 'car', 'cat', 'chair',
+           'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant',
+           'sheep', 'sofa', 'train', 'tvmonitor')
+
+NETS = {'vgg16': ('VGG16',
+                  'VGG16_faster_rcnn_final.caffemodel'),
+        'zf': ('ZF',
+                  'ZF_faster_rcnn_final.caffemodel')}
+
+
+def process_frame(frame_idx, path, csvfile):
+    print "Processing %s in %s" % (frame_idx, path)
+    img = cv2.imread(path)
+    scores, boxes = im_detect(net, img)
+
+    CONF_THRESH = 0.8
+    NMS_THRESH = 0.3
+    for cls_ind, cls in enumerate(CLASSES[1:]):
+        cls_ind += 1 # because we skipped background
+        cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+        cls_scores = scores[:, cls_ind]
+        dets = np.hstack((cls_boxes,
+                          cls_scores[:, np.newaxis])).astype(np.float32)
+        keep = nms(dets, NMS_THRESH)
+        dets = dets[keep, :]
+        #vis_detections(im, cls, dets, thresh=CONF_THRESH)
+        for row in range(dets.shape[0]):
+            csvfile.writerow([frame_idx, row, cls, dets[row, -1]] + list(dets[row,:4]))
+
+if __name__ == '__main__':
+    cfg.TEST.HAS_RPN = True  # Use RPN for proposals
+
+    args = JArgs()
+
+    prototxt = os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0],
+                            'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
+    caffemodel = os.path.join(cfg.DATA_DIR, 'faster_rcnn_models',
+                              NETS[args.demo_net][1])
+
+    if not os.path.isfile(caffemodel):
+        raise IOError(('{:s} not found.\nDid you run ./data/script/'
+                       'fetch_faster_rcnn_models.sh?').format(caffemodel))
+
+    if args.cpu_mode:
+        caffe.set_mode_cpu()
+    else:
+        caffe.set_mode_gpu()
+        caffe.set_device(args.gpu_id)
+        cfg.GPU_ID = args.gpu_id
+    net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+
+
+    print '\n\nLoaded network {:s}'.format(caffemodel)
+
+    # Warmup on a dummy image
+    im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
+    for i in xrange(2):
+        _, _= im_detect(net, im)
+
+    # im_names = ['000456.jpg', '000542.jpg', '001150.jpg',
+    #             '001763.jpg', '004545.jpg']
+    # for im_name in im_names:
+    #     print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+    #     print 'Demo for data/demo/{}'.format(im_name)
+    #     demo(net, im_name)
+    with open("/frames/rcnn.csv", "wb") as csvfile:
+        writer = csv.writer(csvfile)
+        for im in os.listdir("/frames"):
+            name, ext = os.path.splitext(im)
+            if ext == ".jpg":
+                frame = int(re.match(r".*?(\d+)", name).groups()[0])
+                process_frame(frame, os.path.join("/frames", im), writer)
--- src/extract_frames.jl
+++ src/extract_frames.jl
@@ -1,8 +1,8 @@
-basedir = "/storage/malmaud/kinect"
+basedir = "/storage/malmaud/kinect2"
 @sync for dir in readdir(basedir)
     isdir(joinpath(basedir, dir)) || continue
     for channel in ["color", "depth"]
-        base = joinpath(basedir, dir, channel)        
+        base = joinpath(basedir, dir, channel)
         isdir(joinpath(base, "frames")) || mkdir(joinpath(base, "frames"))
         @async run(`ffmpeg -i $(joinpath(base, "movie.avi")) -r 30/1 $(joinpath(base, "frames/output%03d.jpg"))`) # TODO change to %04d
     end
